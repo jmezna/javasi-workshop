eae2292c0fd22f7d78f505e7e888fe1bc2dd626c
Jan Meznarič <jmezna@gmail.com>
2017-10-16 18:39:03 +0200

    base project

---
+++ customers/services/src/main/java/com/kumuluz/ee/samples/tutorial/customers/services/producers/PersistenceProducer.java
@@ -0,0 +1,31 @@
+package com.kumuluz.ee.samples.tutorial.customers.services.producers;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+/**
+ * @author Tilen Faganel, Sunesis ltd.
+ * @since 1.0.0
+ */
+public class PersistenceProducer {
+
+    @PersistenceUnit(unitName = "customers-jpa")
+    private EntityManagerFactory emf;
+
+    @Produces
+    @RequestScoped
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void disposeEntityManager(@Disposes EntityManager entityManager) {
+
+        if (entityManager.isOpen()) {
+            entityManager.close();
+        }
+    }
+}
---
+++ orders/api/src/main/java/com/kumuluz/ee/samples/tutorial/orders/api/v1/OrdersApplication.java
@@ -0,0 +1,8 @@
+package com.kumuluz.ee.samples.tutorial.orders.api.v1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1")
+public class OrdersApplication extends Application {
+}
